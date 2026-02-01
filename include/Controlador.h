35892a1365fba85a4fe414d39f8bc12559d23262
Grexcon <davidgu70014@gmail.com>
2026-02-01 01:54:45 -0600

    Clase controlador

---
+++ include/Controlador.h
@@ -0,0 +1,16 @@
+#ifndef CONTROLADOR_H
+#define CONTROLADOR_H
+
+
+class Controlador
+{
+    public:
+        Controlador();
+        virtual ~Controlador();
+
+    protected:
+
+    private:
+};
+
+#endif // CONTROLADOR_H
